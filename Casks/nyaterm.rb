e12fd92b0cb3f31990500dbe17b36876e5398f55
nyakang <61106285+nyakang@users.noreply.github.com>
2026-08-04 14:09:22 +0000

    chore: update nyaterm cask to 1.2.0

--- Casks/nyaterm.rb
+++ Casks/nyaterm.rb
@@ -1,9 +1,9 @@
 cask "nyaterm" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.1.19"
-  sha256 arm:   "35e059851f045a341836c80b6cc7bd7fcbbf160d9751086b8265a7829de480bc",
-         intel: "2b85189bf9f3760720acae11dbb0baf4e496d2a606101b0b8ee5f9f7b4d541a5"
+  version "1.2.0"
+  sha256 arm:   "43efb3d169b50e69c8d72bd24504cf34bdde172ebd03589073c406215fab8c5b",
+         intel: "eec044e8478292fccb92aa05995c0182ea4eb8fb5d3ee5e8e54b71bf7e4954ef"
 
   url "https://github.com/nyakang/nyaterm/releases/download/v#{version}/NyaTerm_#{version}_macos_#{arch}.dmg",
       verified: "github.com/nyakang/nyaterm/"
